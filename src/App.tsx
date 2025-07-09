64ce3d05b8ed6c7324e8358f9d80d5694387f194
andreabastidasc <andrea.bcolina@gmail.com>
2025-07-09 15:15:44 -0300

    scalfold

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="min-h-screen bg-orange-50 flex items-center justify-center">
+            <h1 className="text-4xl font-bold text-orange-600">
+                ¡VainaApp con Tailwind funcionando!
+            </h1>
+        </div>
+    );
+}
+
+export default App;
